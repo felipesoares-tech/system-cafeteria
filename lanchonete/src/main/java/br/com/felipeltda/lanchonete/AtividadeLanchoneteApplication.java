53d53829120fbb5596ebe5a01ac215f218b41c1d
Felipe Soares <soares.felipe200@gmail.com>
2022-09-29 20:48:28 -0300

    incluindo gitignore

---
+++ lanchonete/src/main/java/br/com/felipeltda/lanchonete/AtividadeLanchoneteApplication.java
@@ -0,0 +1,13 @@
+package br.com.felipeltda.lanchonete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeLanchoneteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeLanchoneteApplication.class, args);
+	}
+
+}
---
+++ lanchonete/src/test/java/br/com/felipeltda/lanchonete/AtividadeLanchoneteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.felipeltda.lanchonete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeLanchoneteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
