0cbbab40683a02f81d37761f11560d0eaaba9b21
Felipe Soares <soares.felipe200@gmail.com>
2022-10-14 14:47:51 -0300

    removendo dados do import sql

--- sistema/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into produto (valor_unitario, quantidade, nome) values ('2', '50', 'agua');
-insert into produto (valor_unitario, quantidade, nome) values ('5', '48', 'refrigerante');
-insert into produto (valor_unitario, quantidade, nome) values ('8', '25', 'cerveja');
-insert into produto (valor_unitario, quantidade, nome) values ('3', '33', 'salgadinho');
