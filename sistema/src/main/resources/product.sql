3a07a6464bf3d2e4ca1fcaccae06fa085c32b621
Felipe <soares.felipe200@gmail.com>
2022-11-18 14:43:20 -0300

    removendo bug do banco de dados e alterando configuracao da aplicacao

---
+++ sistema/src/main/resources/product.sql
@@ -0,0 +1,10 @@
+insert into product (nome,quantidade,valor_unitario) values('COCA COLA LATA 350ML', '50', '4.25');
+insert into product (nome,quantidade,valor_unitario) values('CHOCOLATE KIT KAT WHITE NESTLE 41,5G', '100', '3.99');
+insert into product (nome,quantidade,valor_unitario) values('CHOCOLATE KIT KAT 3 FINGERS CAPPUCCINO 41,5G', '100', '3.99');
+insert into product (nome,quantidade,valor_unitario) values('CAIXA DE BALA DROPS HALLS SABOR CEREJA C/21 UNID', '100', '39.95');
+insert into product (nome,quantidade,valor_unitario) values('SALGADINHO 300G DORITOS NACHO', '150', '21.89');
+insert into product (nome,quantidade,valor_unitario) values('BATATA FRITA ONDULADA ORIGINAL RUFFLES PACOTE 115G', '150', '9.99');
+insert into product (nome,quantidade,valor_unitario) values('BISCOITO RECHEADO DANIX SABOR CHOCOLATE 130G', '40', '3.49');
+insert into product (nome,quantidade,valor_unitario) values('BISCOITO RECHEADO DANIX VITALICIO SABOR LEITE 86G', '40', '2.19');
+insert into product (nome,quantidade,valor_unitario) values('AMENDOIM DORI JAPONES 200G', '45', '3.57');
+insert into product (nome,quantidade,valor_unitario) values('CAIXA BOMBOM ESPECIALIDADES NESTLE 251G', '10', '13.99');
