eab5f94457cc9d6c94cfd286e4ecc29aff774f49
Felipe Soares <soares.felipe200@gmail.com>
2022-11-28 22:04:41 -0300

    adicionando atualizar parcial para produto

--- sistema/src/main/java/br/com/felipeltda/lanchonete/api/controller/ProductController.java
+++ sistema/src/main/java/br/com/felipeltda/lanchonete/api/controller/ProductController.java
@@ -2,13 +2,17 @@ package br.com.felipeltda.lanchonete.api.controller;
 import br.com.felipeltda.lanchonete.domain.model.Product;
 import br.com.felipeltda.lanchonete.domain.repository.ProductRepository;
 import br.com.felipeltda.lanchonete.domain.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -51,6 +55,28 @@ public class ProductController {
             return ResponseEntity.status(HttpStatus.OK).body(saveProduct);
         } else
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("test");
+    }
+
+    private void merge(Map<String, Object> sourceData, Product targetProduct) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Product sourceProduct = objectMapper.convertValue(sourceData, Product.class);
+
+        sourceData.forEach((nomePropriedade, valorPropriedade) -> {
+            Field field = ReflectionUtils.findField(Product.class, nomePropriedade);
+            field.setAccessible(true);
+
+            Object newValue = ReflectionUtils.getField(field, sourceProduct);
+
+            ReflectionUtils.setField(field, targetProduct, newValue);
+        });
+    }
+
+    @PatchMapping("/{productId}")
+    public ResponseEntity<?> partialUpdate(@PathVariable Long productId, @RequestBody Map<String, Object> fields){
+        Optional<Product> currentProduct = productRepository.findById(productId);
+        currentProduct.ifPresent(product -> merge(fields, product));
+
+        return updateProduct(productId,currentProduct.get());
     }
 
 }
