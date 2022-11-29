c5b856e64d07bdbc2c58b1ed60b30a528927c091
Felipe Soares <soares.felipe200@gmail.com>
2022-11-28 22:19:44 -0300

    adicionando atualizar parcial para atendente

--- sistema/src/main/java/br/com/felipeltda/lanchonete/api/controller/AttendantController.java
+++ sistema/src/main/java/br/com/felipeltda/lanchonete/api/controller/AttendantController.java
@@ -3,16 +3,21 @@ package br.com.felipeltda.lanchonete.api.controller;
 import br.com.felipeltda.lanchonete.domain.exception.EntityNotFoundException;
 import br.com.felipeltda.lanchonete.domain.exception.LinkedEntityException;
 import br.com.felipeltda.lanchonete.domain.model.Attendant;
+import br.com.felipeltda.lanchonete.domain.model.Client;
 import br.com.felipeltda.lanchonete.domain.repository.AttendantRepository;
 import br.com.felipeltda.lanchonete.domain.service.AttendantService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -55,6 +60,28 @@ public class AttendantController {
             return ResponseEntity.status(HttpStatus.OK).body(saveAttendant);
         } else
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("test");
+    }
+
+    private void merge(Map<String, Object> sourceData, Attendant targetAttendant) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Attendant sourceAttendant = objectMapper.convertValue(sourceData, Attendant.class);
+
+        sourceData.forEach((nomePropriedade, valorPropriedade) -> {
+            Field field = ReflectionUtils.findField(Attendant.class, nomePropriedade);
+            field.setAccessible(true);
+
+            Object newValue = ReflectionUtils.getField(field, sourceAttendant);
+
+            ReflectionUtils.setField(field, targetAttendant, newValue);
+        });
+    }
+
+    @PatchMapping("/{attendantId}")
+    public ResponseEntity<?> partialUpdate(@PathVariable String attendantId, @RequestBody Map<String, Object> fields){
+        Optional<Attendant> currentAttendant = attendantRepository.findById(attendantId);
+        currentAttendant.ifPresent(attendant -> merge(fields, attendant));
+
+        return updateAttendant(attendantId,currentAttendant.get());
     }
 }
 
